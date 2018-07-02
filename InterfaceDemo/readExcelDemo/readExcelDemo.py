81021aa8b2796e84f65401e3083ff48a3d089185
cjdxsxd <3360048@163.com>
2018-07-02 11:20:26 +0800

    修改

--- InterfaceDemo/readExcelDemo/readExcelDemo.py
+++ InterfaceDemo/readExcelDemo/readExcelDemo.py
@@ -5,26 +5,32 @@
 import xlrd
 import os
 
-print(os.getcwd())
-file_path = os.path.join(os.getcwd(), '企业类型ID.xlsx')
-print(file_path)
 
-excel_file = xlrd.open_workbook(file_path)
-# 返回所有sheetname
-sheet_name = excel_file.sheet_names()
-print(sheet_name)
-# 指定某个sheet,打印行数，列数
-sheet_one = excel_file.sheet_by_name('Sheet1')
-print(sheet_one.name, sheet_one.nrows, sheet_one.ncols)
+def get_cell_value(names, rows, cols):
+    file_path = os.path.join(os.getcwd(), '企业类型数据字典（21301）.xlsx')
+    print(file_path)
 
-# 获取整行，整列值
-rows_value = sheet_one.row_values(2)
-print(rows_value)
+    excel_file = xlrd.open_workbook(file_path)
+    # 返回所有sheetname
+    sheet_name = excel_file.sheet_names()
+    print(sheet_name)
+    # 指定某个sheet,打印行数，列数
+    # sheet_one = excel_file.sheet_by_name('Sheet1')
+    sheet_one = excel_file.sheet_by_name(names)
+    print(sheet_one.name, sheet_one.nrows, sheet_one.ncols)
 
-cols_value = sheet_one.col_values(2)
-print(cols_value)
+    # 获取整行，整列值
+    rows_value = sheet_one.row_values(1)
+    print(rows_value)
+
+    cols_value = sheet_one.col_values(1)
+    print(cols_value)
 
 
-# 读取某个值
-cell_value = sheet_one.cell_value(2, 2)
-print(cell_value)
+    # 读取某个值
+    cell_value = sheet_one.cell_value(rows, cols)
+    print(cell_value)
+
+
+if __name__ == '__main__':
+    get_cell_value('企业类型', 1, 1)
